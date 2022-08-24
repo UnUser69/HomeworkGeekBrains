b99f37b8f7c2a17c79db6416265496fd2bad00af
UnUser69 <chepelev.mark@mail.ru>
2022-08-24 14:19:08 +0300

    Add comments for first ex.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,33 @@
-﻿int num;
+﻿/*
+// Write a program that takes two numbers as input and outputs which number is greater and which is less. //
+// Input of variables. //
+int num;
 int num2;
-
+// Console displays a message. //
 Console.Write("Enter the first number: ");
-
+// The variable "num" is assigned a value. //
 num = Convert.ToInt32(Console.ReadLine());
-
+// Console displays a message. //
 Console.Write("Enter the second number: ");
-
+// The variable "num2" is assigned a value. //
 num2 = Convert.ToInt32(Console.ReadLine());
-
+// The variable "maxNumber" is entered with the function "Math.Max". //
 int maxNumber = Math.Max(num, num2);
-
+// Console displays a message. //
 Console.Write("The maximum number is the number: ");
-
+// Console displays a message. //
 Console.WriteLine(maxNumber);
-
+// The variable "minNumber" is entered with the function "Math.Min". //
 int minNumber = Math.Min(num, num2);
-
+// Console displays a message. //
 Console.Write("The minimum number is the number: ");
-
+// Console displays a message. //
 Console.WriteLine(minNumber);
 
+*/
+
+/*
+
+
+
+*/
