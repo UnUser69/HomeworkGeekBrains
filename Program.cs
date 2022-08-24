f45d7755af088477fe579075660aebfb8a641543
UnUser69 <chepelev.mark@mail.ru>
2022-08-24 13:42:08 +0300

    Create the first ex.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num;
+int num2;
+
+Console.Write("Enter the first number: ");
+
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+
+num2 = Convert.ToInt32(Console.ReadLine());
+
+int maxNumber = Math.Max(num, num2);
+
+Console.Write("The maximum number is the number: ");
+
+Console.WriteLine(maxNumber);
+
+int minNumber = Math.Min(num, num2);
+
+Console.Write("The minimum number is the number: ");
+
+Console.WriteLine(minNumber);
+
