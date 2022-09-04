5cd9ff8655ebee95ea1228c9d915a57df681a375
UnUser69 <chepelev.mark@mail.ru>
2022-09-04 14:40:27 +0300

    First exercise done.

---
+++ Homework4/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// Displaying a message on the screen. //
+Console.Write("Enter natural number: ");
+// Entering a variable denoting the number. //
+int number = Convert.ToInt32(Console.ReadLine());
+// Displaying a message on the screen. //
+Console.Write("Enter the degree of a natural number: ");
+// Entering a variable denoting the degree. //
+int gradd = Convert.ToInt32(Console.ReadLine());
+// The variable of the desired answer is entered. //
+int desired = 1;
+// The "for" loop is executed for raising a natural number to a power. //
+for(int counter=1; counter<=gradd; counter++){
+    // Exponentiation. //
+    desired*=number;
+}
+// Displaying a message on the screen. //
+Console.WriteLine($"The answer is {desired}.");
+
